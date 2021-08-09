da7aa7163778b5d9aaf9f0e6ffa542dbde8b08da
Joao Victor Galindo <doublito@gmail.com>
2021-08-09 17:54:08 -0300

    Criado virtual environment 'env'

---
+++ ProgWebG1/ProgWebG1/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Projetos\Fkl\INF1407-Prog-Web\ProgWebG1\ProgWebG1\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
